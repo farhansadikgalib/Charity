0c8afe5aa84a7498d7b9507806aa1c6bdaed8bd4
Sakib Hossain <Sakibhossain321@gmail.com>
2020-03-09 17:47:19 +0600

    new

--- app/src/main/java/com/farhan/charity/DashBoard.java
+++ app/src/main/java/com/farhan/charity/DashBoard.java
@@ -5,25 +5,83 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.models.SlideModel;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.github.mikephil.charting.utils.ColorTemplate.rgb;
 
 public class DashBoard extends AppCompatActivity {
+
+
+    public static final int[] colordata = {
+            rgb("#FF2531"), rgb("#0B287B"), rgb("#02AFAE"), rgb("#FEA200")};
+
+    public static final int colorwhite = rgb("#ffffff");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
 
+        imageSlider();
+        pieChartFuntion();
 
+
+    }
+
+
+    public void imageSlider() {
         ImageSlider imageSlider = findViewById(R.id.slider);
 
         List<SlideModel> slideModels = new ArrayList<>();
-        slideModels.add(new SlideModel(R.drawable.dashboard_cp1_new));
-        slideModels.add(new SlideModel(R.drawable.dashboard_cp1_new));
-        slideModels.add(new SlideModel(R.drawable.dashboard_cp1_new));
-        slideModels.add(new SlideModel(R.drawable.dashboard_cp1_new));
+        slideModels.add(new SlideModel(R.drawable.coverpic_001));
+        slideModels.add(new SlideModel(R.drawable.coverpic_001));
+        slideModels.add(new SlideModel(R.drawable.coverpic_001));
+        slideModels.add(new SlideModel(R.drawable.coverpic_001));
         imageSlider.setImageList(slideModels, true);
+
     }
+
+
+    public void pieChartFuntion() {
+
+                PieChart pieChart = findViewById(R.id.piechart);
+                PieDataSet pieDataSet = new PieDataSet(getData(), "");
+
+                pieChart.setDrawEntryLabels(true);
+                pieDataSet.setColors(colordata);
+                PieData pieData = new PieData(pieDataSet);
+
+                pieChart.setHoleRadius(40);
+                pieChart.setUsePercentValues(true);
+                pieData.setValueTextSize(13f);
+                pieChart.setEntryLabelTextSize(8f);
+                pieChart.setEntryLabelColor(colorwhite);
+                pieData.setValueTextColor(colorwhite);
+                pieChart.setTransparentCircleRadius(0);
+                pieChart.setData(pieData);
+                pieChart.animateXY(3000, 3000);
+                pieChart.invalidate();
+
+            }
+
+            private ArrayList getData() {
+                ArrayList<PieEntry> entries = new ArrayList<>();
+                entries.add(new PieEntry(30, "শিক্ষা সংক্রান্ত"));
+                entries.add(new PieEntry(40, "প্রতিবন্ধী সংক্রান্ত"));
+                entries.add(new PieEntry(60, " স্বাস্থ্য সংক্রান্ত"));
+                entries.add(new PieEntry(70, "দুর্যোগ সংক্রান্ত"));
+                return entries;
+            }
+
+
 }
+
+
+
--- app/src/main/java/com/farhan/charity/ProfilewithPermanentAddressActivity.java
+++ app/src/main/java/com/farhan/charity/ProfilewithPermanentAddressActivity.java
@@ -1,14 +1,75 @@
 package com.farhan.charity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class ProfilewithPermanentAddressActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_with_permanent_address);
+
+
+
+        tabLayout= findViewById(R.id.tabLayout1);
+        viewPager= findViewById(R.id.viewPager);
+        //
+        //        i1= findViewById(R.id.item1);
+//        i2= findViewById(R.id.item2);
+//        i3= findViewById(R.id.item3);
+
+
+
+
+        tabLayout.addTab(tabLayout.newTab().setText("cadadcac"));
+        tabLayout.addTab(tabLayout.newTab().setText("asds"));
+//        tabLayout.addTab(tabLayout.newTab().setText("asd"));
+////        tabLayout.setTabTextColors(
+//                getResources().getColor(R.color.colorPrimary),
+//                getResources().getColor(R.color.colorAccent)
+//        );
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+
+
+
+        final MyAdapter adapter = new MyAdapter(this,getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+
+
     }
+
+
 }
+
