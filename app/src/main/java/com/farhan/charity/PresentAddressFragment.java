9bff3413ddd0a33a222687d2ae8b3c1d3d428b92
Sakib Hossain <Sakibhossain321@gmail.com>
2020-03-10 11:18:44 +0600

    push AGAIN FULL

---
+++ app/src/main/java/com/farhan/charity/PresentAddressFragment.java
@@ -0,0 +1,30 @@
+package com.farhan.charity;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PresentAddressFragment extends Fragment {
+
+
+    public PresentAddressFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_present_address_fragment, container, false);
+    }
+
+}
