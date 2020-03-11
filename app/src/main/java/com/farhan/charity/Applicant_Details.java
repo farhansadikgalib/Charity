8a9ddc8a56a35239ae5d6dc78038841551bb8cff
OLMG geek <mridulnath098@gmail.com>
2020-03-11 13:13:04 +0600

    Some issue solve in FowardForm

--- app/src/main/java/com/farhan/charity/Applicant_Details.java
+++ app/src/main/java/com/farhan/charity/Applicant_Details.java
@@ -1,14 +1,29 @@
 package com.farhan.charity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.farhan.charity.Dashboard_Items.Forward_Application_Activity;
+
 public class Applicant_Details extends AppCompatActivity {
+    Button forwardBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_applicant_details);
+
+        forwardBtn=findViewById(R.id.forwardBtn);
+
+        forwardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Applicant_Details.this, FowardForm.class));
+            }
+        });
     }
 }
