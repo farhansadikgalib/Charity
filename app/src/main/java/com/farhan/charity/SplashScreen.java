b5746f74c85cce9dd88ab873e9a00a82b3107eca
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2020-03-11 14:35:46 +0600

    Login functionality design is completed now.

---
+++ app/src/main/java/com/farhan/charity/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.farhan.charity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    int SPLASH_TIME = 1000;
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },SPLASH_TIME);
+
+    }
+}
