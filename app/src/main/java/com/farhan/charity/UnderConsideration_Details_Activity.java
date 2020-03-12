69896d267fecd3227e4244ad9904692864c27de4
Sakib Hossain <Sakibhossain321@gmail.com>
2020-03-12 12:50:52 +0600

    profile activity connected

--- app/src/main/java/com/farhan/charity/UnderConsideration_Details_Activity.java
+++ app/src/main/java/com/farhan/charity/UnderConsideration_Details_Activity.java
@@ -1,14 +1,30 @@
 package com.farhan.charity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.farhan.charity.Dashboard_Items.ApplicationOf_EducationActivity;
+
 public class UnderConsideration_Details_Activity extends AppCompatActivity {
+
+    TextView applicantsNameTV1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_under_consideration__details_);
+
+        applicantsNameTV1 = findViewById(R.id.applicantsNameTV);
+
+        applicantsNameTV1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(UnderConsideration_Details_Activity.this, ProfilewithPermanentAddressActivity.class));
+            }
+        });
     }
 }
