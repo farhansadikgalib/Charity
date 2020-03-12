d72fb1559abc4113427f9049ba90d426e102f941
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2020-03-12 10:20:47 +0600

    application Unapproved is complete.

--- app/src/main/java/com/farhan/charity/DashbordAdapter/Dashbor_subItem/UnapprovedApplication.java
+++ app/src/main/java/com/farhan/charity/DashbordAdapter/Dashbor_subItem/UnapprovedApplication.java
@@ -1,16 +1,45 @@
 package com.farhan.charity.DashbordAdapter.Dashbor_subItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.farhan.charity.Adapter.Unapproved_Adapter;
+import com.farhan.charity.Model.ItemModel;
 import com.farhan.charity.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UnapprovedApplication extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    List<ItemModel> items;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unapproved_application);
+        items = new ArrayList<>();
+        getData();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(UnapprovedApplication.this);
+        recyclerView = findViewById(R.id.recyclerView_unapproved);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        Unapproved_Adapter adapter = new Unapproved_Adapter(UnapprovedApplication.this, items);
+        recyclerView.setAdapter(adapter);
+    }
+    public void getData(){
+
+        items.add(new ItemModel(R.mipmap.ic_launcher,"Farhan","শিক্ষা আবেদন","৫০০০০০৯"));
+        items.add(new ItemModel(R.mipmap.ic_launcher,"Farhan","শিক্ষা আবেদন","৫০০০০০৯"));
+        items.add(new ItemModel(R.mipmap.ic_launcher,"Farhan","শিক্ষা আবেদন","৫০০০০০৯"));
+        items.add(new ItemModel(R.mipmap.ic_launcher,"Farhan","শিক্ষা আবেদন","৫০০০০০৯"));
+        items.add(new ItemModel(R.mipmap.ic_launcher,"Farhan","শিক্ষা আবেদন","৫০০০০০৯"));
+        items.add(new ItemModel(R.mipmap.ic_launcher,"Farhan","শিক্ষা আবেদন","৫০০০০০৯"));
+        items.add(new ItemModel(R.mipmap.ic_launcher,"Farhan","শিক্ষা আবেদন","৫০০০০০৯"));
+        items.add(new ItemModel(R.mipmap.ic_launcher,"Farhan","শিক্ষা আবেদন","৫০০০০০৯"));
+
+
     }
 }
